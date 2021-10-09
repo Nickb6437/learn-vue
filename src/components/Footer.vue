97e46ba6689b5714f7fd0aa4b3248c12321f0435
nickb6437 <Nickb6437@outlook.com>
2021-10-09 11:36:59 +0100

    Initial Commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <p>Copyright &copy; 2021</p>
+    <router-link to="/about">About</router-link>
+  </footer>
+</template>
+
+<style scoped>
+a {
+  color: #333;
+}
+footer {
+  margin: 2rem 0 0 0;
+  text-align: center;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <Task
+      @toggle-reminder="$emit('toogle-reminder', task.id)"
+      @delete-task="$emit('delete-task', task.id)"
+      :task="task"
+    />
+  </div>
+</template>
+
+<script>
+import Task from "./Task";
+
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+  emits: ["delete-task", "toogle-reminder"],
+};
+</script>
+
+<style scoped></style>
